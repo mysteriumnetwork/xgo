18ab92148de97b6b401b6a5dd29ae5d77f3ae52c
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-11-26 01:39:36 +0200

    update tests

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 set -e
 
 # List of comma separated targets.
-TARGETS="linux/amd64,darwin/amd64,windows/amd64"
+TARGETS="linux/amd64,darwin/amd64,darwin/arm64,windows/amd64"
 
 docker run --rm \
     -v "$PWD"/build:/build \
@@ -14,7 +14,7 @@ docker run --rm \
     -e FLAG_RACE=false \
     -e FLAG_LDFLAGS="-s -w" \
     -e TARGETS=$TARGETS \
-    mysteriumnetwork/xgo:1.13.6 ./tests/embedded_c/.
+    mysteriumnetwork/xgo:1.17.3 ./tests/embedded_c/.
 
 docker run --rm \
     -v "$PWD"/build:/build \
@@ -26,4 +26,4 @@ docker run --rm \
     -e FLAG_RACE=false \
     -e FLAG_BUILDMODE=default \
     -e TARGETS=$TARGETS \
-    mysteriumnetwork/xgo:1.13.6 ./tests/embedded_cpp/.
+    mysteriumnetwork/xgo:1.17.3 ./tests/embedded_cpp/.
